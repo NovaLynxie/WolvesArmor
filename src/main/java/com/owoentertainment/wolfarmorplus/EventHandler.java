7e640357b1b621c4cb25896c24c8c91aa5aeb9b3
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-16 15:29:23 +0000

    Implement mod event handler

--- src/main/java/com/owoentertainment/wolfarmorplus/EventHandler.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/EventHandler.java
@@ -3,16 +3,28 @@ package com.owoentertainment.wolfarmorplus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
 //import net.minecraft.entity.passive.WolfEntity; // missing or no longer exists in newer builds
 //import net.minecraft.entity.player.PlayerEntity; // missing or no longer exists in newer builds
 //import net.minecraft.world.World; // missing or no longer exists in newer builds
 
-public class EventHandlers {
+public class EventHandler {
     @SubscribeEvent
     public void interactWolf(PlayerInteractEvent.EntityInteractSpecific event) {
         // get player entity from interact event
         Player player = event.getEntity();
-        //PlayerEntity player = event.getTarget();
+        // Check if entity interacted with is a wolf entity.
+        if (event.getTarget() instanceof Wolf) {
+            // if true, get entity data
+            Wolf wolf = (Wolf) event.getTarget();
+            // Check if wolf is owned by player, but first check if wolf owner uuid is not null.
+            if (wolf.getOwnerUUID() != null) {
+                if (wolf.getOwnerUUID().equals(player.getUUID())) {
+                    // TODO: Implement WolfArmorAccessor API
+                    WolfArmorPlus.LOGGER.debug("Player owns wolf!");
+                }
+            }
+        }
     }
 }
--- src/main/java/com/owoentertainment/wolfarmorplus/WolfArmorPlus.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/WolfArmorPlus.java
@@ -1,5 +1,23 @@
 package com.owoentertainment.wolfarmorplus;
 
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(WolfArmorPlus.MOD_ID)
 public class WolfArmorPlus {
-    
+    // Define mod id to be used globally throughout the code.
+    public static final String MOD_ID = "wolfarmorplus";
+    // Define mod logger to be used globally throughout the code.
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // Main constructor for mod class, this is first run when mod is loaded.
+    public WolfArmorPlus(IEventBus modEventBus, ModContainer modContainer) {
+        // Register mod events here.
+        NeoForge.EVENT_BUS.register(new EventHandler());
+    }
 }
