d502bbb3902702ef66cfbfcefdc444e5f559c7c8
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-18 23:05:45 +0000

    Removed unused classes.

--- src/main/java/com/owoentertainment/wolfarmorplus/api/WolfArmorAccessor.java
+++
@@ -1,9 +0,0 @@
-package com.owoentertainment.wolfarmorplus.api;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface WolfArmorAccessor { 
-    // Code crashes here calling wolf to accessor, workaround required
-    ItemStack getWolfArmor();
-    void setWolfArmor(ItemStack stack);
-}
