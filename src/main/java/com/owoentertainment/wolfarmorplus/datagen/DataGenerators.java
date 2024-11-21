03e72a59c4299aef1e00128fd108fc787a902041
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-21 13:11:01 +0000

    Add DataGenerators main class

---
+++ src/main/java/com/owoentertainment/wolfarmorplus/datagen/DataGenerators.java
@@ -0,0 +1,18 @@
+package com.owoentertainment.wolfarmorplus.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        // Add our mod's model providers
+        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
+    }
+}
