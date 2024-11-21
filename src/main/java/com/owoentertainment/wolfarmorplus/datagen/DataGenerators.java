bd872cde323be2dccb0dc374d836042db1141f76
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-21 14:37:32 +0000

    Add EventBusSubscriber notations.

--- src/main/java/com/owoentertainment/wolfarmorplus/datagen/DataGenerators.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/datagen/DataGenerators.java
@@ -1,11 +1,15 @@
 package com.owoentertainment.wolfarmorplus.datagen;
 
+import com.owoentertainment.wolfarmorplus.WolfArmorPlus;
+
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
+@EventBusSubscriber(modid = WolfArmorPlus.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
