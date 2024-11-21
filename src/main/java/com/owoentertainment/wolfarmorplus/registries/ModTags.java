b6f403aa73170222528e49436bce0d759b1d2317
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-21 15:49:55 +0000

    Add mod tags

---
+++ src/main/java/com/owoentertainment/wolfarmorplus/registries/ModTags.java
@@ -0,0 +1,16 @@
+package com.owoentertainment.wolfarmorplus.registries;
+
+import com.owoentertainment.wolfarmorplus.WolfArmorPlus;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class ModTags {
+    public static class Items {
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(WolfArmorPlus.MODID, name));
+        }
+    }
+}
