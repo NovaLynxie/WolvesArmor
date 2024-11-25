453e6ffbbf24eb3fad762e8facdb93fedc0b0df4
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-25 14:42:40 +0000

    Remove unused or commented out imports.

--- src/main/java/com/owoentertainment/wolfarmorplus/datagen/ModItemModelProvider.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/datagen/ModItemModelProvider.java
@@ -7,13 +7,10 @@ import com.owoentertainment.wolfarmorplus.registries.ItemRegistry;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceKey;
-//import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.armortrim.TrimMaterial;
 import net.minecraft.world.item.armortrim.TrimMaterials;
-//import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-//import net.neoforged.neoforge.registries.DeferredItem;
 
 public class ModItemModelProvider extends ItemModelProvider {
     private static LinkedHashMap<ResourceKey<TrimMaterial>, Float> trimMaterials = new LinkedHashMap<>();
