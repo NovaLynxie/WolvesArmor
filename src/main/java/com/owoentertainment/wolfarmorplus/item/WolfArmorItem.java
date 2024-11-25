e1b936893c291bb7c64d9953c035a1377db5a708
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-25 16:42:48 +0000

    Removed unused item class.

--- src/main/java/com/owoentertainment/wolfarmorplus/item/WolfArmorItem.java
+++
@@ -1,9 +0,0 @@
-package com.owoentertainment.wolfarmorplus.item;
-
-import net.minecraft.world.item.Item;
-
-public class WolfArmorItem extends Item {
-    public WolfArmorItem(Properties properties) {
-        super(properties);
-    }
-}
