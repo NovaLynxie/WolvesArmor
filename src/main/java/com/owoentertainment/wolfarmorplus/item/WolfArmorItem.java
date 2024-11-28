e030d006fa7eea2b4ea2e56aeb59d9e4689caa32
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-28 20:40:47 +0000

    Add custom wolf armor class.

---
+++ src/main/java/com/owoentertainment/wolfarmorplus/item/WolfArmorItem.java
@@ -0,0 +1,29 @@
+package com.owoentertainment.wolfarmorplus.item;
+
+import com.owoentertainment.wolfarmorplus.WolfArmorPlus;
+
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.AnimalArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class WolfArmorItem extends AnimalArmorItem {
+    private final ResourceLocation textureLocation;
+
+    public WolfArmorItem(Holder<ArmorMaterial> armorMaterial, Properties properties) {
+        super(armorMaterial, AnimalArmorItem.BodyType.CANINE, true, properties);
+        String materialHolderName = armorMaterial.getRegisteredName();
+        ResourceLocation resourceLocation = getArmorTexture(materialHolderName.substring(materialHolderName.lastIndexOf(":") + 1));
+        WolfArmorPlus.LOGGER.debug(resourceLocation.toString());
+        this.textureLocation = resourceLocation.withSuffix(".png");
+    }
+    @Override
+    public ResourceLocation getTexture() {
+        return this.textureLocation; // get mod's custom armor texture instead
+    }
+
+    public ResourceLocation getArmorTexture(String materialName) {
+        String armorTexture = "textures/entity/wolf/wolf_armor_"+ materialName;
+        return ResourceLocation.fromNamespaceAndPath(WolfArmorPlus.MODID, armorTexture);
+    }
+}
