a1c2ca56d5e66b839fa73afff9ec0eb07e38d829
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2024-11-28 21:41:03 +0000

    Use ArmorItem BODY durability factors instead.

--- src/main/java/com/owoentertainment/wolfarmorplus/registries/ModItems.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/registries/ModItems.java
@@ -3,6 +3,7 @@ package com.owoentertainment.wolfarmorplus.registries;
 import com.owoentertainment.wolfarmorplus.WolfArmorPlus;
 import com.owoentertainment.wolfarmorplus.item.WolfArmorItem;
 
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
@@ -14,34 +15,34 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WolfArmorPlus.MODID);
     // Register our armor items here
     public static final DeferredItem<Item> LEATHER_WOLF_ARMOR = ITEMS.register("leather_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.LEATHER, new Item.Properties().durability(24).stacksTo(1))
+        ArmorMaterials.LEATHER, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(1)).stacksTo(1))
     );
     public static final DeferredItem<Item> MOSSY_WOLF_ARMOR = ITEMS.register("mossy_wolf_armor", () -> new WolfArmorItem(
         WolfArmorMaterials.MOSS, new Item.Properties().stacksTo(1))
     );
     public static final DeferredItem<Item> BONE_WOLF_ARMOR = ITEMS.register("bone_wolf_armor", () -> new WolfArmorItem(
-        WolfArmorMaterials.BONE, new Item.Properties().durability(32).stacksTo(1))
+        WolfArmorMaterials.BONE, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(2)).stacksTo(1))
     );
     //public static final DeferredItem<Item> COPPER_WOLF_ARMOR = ITEMS.register("copper_wolf_armor", () -> new WolfArmorItem(
-    //    WolfArmorMaterials.COPPER, WolfArmorItem.BodyType.CANINE, true, new Item.Properties().durability(48).stacksTo(1))
+    //    WolfArmorMaterials.COPPER, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(2)).stacksTo(1))
     //);
     public static final DeferredItem<Item> TURTLE_WOLF_ARMOR = ITEMS.register("turtle_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.TURTLE, new Item.Properties().durability(128).stacksTo(1))
+        ArmorMaterials.TURTLE, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(4)).stacksTo(1))
     );
     public static final DeferredItem<Item> IRON_WOLF_ARMOR = ITEMS.register("iron_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.IRON, new Item.Properties().durability(72).stacksTo(1))
+        ArmorMaterials.IRON, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(5)).stacksTo(1))
     );
     public static final DeferredItem<Item> CHAIN_WOLF_ARMOR = ITEMS.register("chain_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.CHAIN, new Item.Properties().durability(48).stacksTo(1))
+        ArmorMaterials.CHAIN, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(5)).stacksTo(1))
     );
     public static final DeferredItem<Item> GOLD_WOLF_ARMOR = ITEMS.register("gold_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.GOLD, new Item.Properties().durability(16).stacksTo(1))
+        ArmorMaterials.GOLD, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(2)).stacksTo(1))
     );
     public static final DeferredItem<Item> DIAMOND_WOLF_ARMOR = ITEMS.register("diamond_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.DIAMOND, new Item.Properties().durability(256).stacksTo(1))
+        ArmorMaterials.DIAMOND, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(6)).stacksTo(1))
     );
     public static final DeferredItem<Item> NETHERITE_WOLF_ARMOR = ITEMS.register("netherite_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.NETHERITE, new Item.Properties().durability(512).stacksTo(1).fireResistant())
+        ArmorMaterials.NETHERITE, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(8)).stacksTo(1).fireResistant())
     );
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
