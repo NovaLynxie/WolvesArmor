b8b3e36bf82d0d07efcd55fc7a3f941c54eff985
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2025-02-28 11:23:03 +0000

    Disable mossy armor (for now)

--- src/main/java/com/owoentertainment/wolfarmorplus/registries/ModItems.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/registries/ModItems.java
@@ -19,16 +19,18 @@ public class ModItems {
     
     // Custom wolf armors
     public static final DeferredItem<Item> BONE_WOLF_ARMOR = ITEMS.register("bone_wolf_armor", () -> new WolfArmorItem(
-        WolfArmorMaterials.BONE, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(2)).stacksTo(1))
+        WolfArmorMaterials.BONE, 2)
     );
     public static final DeferredItem<Item> CHAIN_WOLF_ARMOR = ITEMS.register("chain_wolf_armor", () -> new WolfArmorItem(
-        WolfArmorMaterials.CHAIN, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(3)).stacksTo(1))
+        WolfArmorMaterials.CHAIN, 3)
     );
+    /* TODO Requires rework to function as unbreakable no protection armor!
     public static final DeferredItem<Item> MOSSY_WOLF_ARMOR = ITEMS.register("mossy_wolf_armor", () -> new WolfArmorItem(
-        WolfArmorMaterials.MOSS, new Item.Properties().stacksTo(1))
+        WolfArmorMaterials.MOSS, 1)
     );
+     */
     public static final DeferredItem<Item> TURTLE_WOLF_ARMOR = ITEMS.register("turtle_wolf_armor", () -> new WolfArmorItem(
-        ArmorMaterials.TURTLE, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(6)).stacksTo(1))
+        ArmorMaterials.TURTLE, 6)
     );
     public static void registerItems(IEventBus eventBus) {
         ITEMS.register(eventBus);
