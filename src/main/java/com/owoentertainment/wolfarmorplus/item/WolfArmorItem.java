65ded64c321b648b622c48cc6a890ba143314035
NovaLynxie <21307676+NovaLynxie@users.noreply.github.com>
2025-02-28 11:22:49 +0000

    Hard code armor properties of wolf custom armor.

--- src/main/java/com/owoentertainment/wolfarmorplus/item/WolfArmorItem.java
+++ src/main/java/com/owoentertainment/wolfarmorplus/item/WolfArmorItem.java
@@ -5,13 +5,16 @@ import com.owoentertainment.wolfarmorplus.WolfArmorPlus;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.AnimalArmorItem;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 
 public class WolfArmorItem extends AnimalArmorItem {
     private final ResourceLocation textureLocation;
 
-    public WolfArmorItem(Holder<ArmorMaterial> armorMaterial, Properties properties) {
-        super(armorMaterial, BodyType.CANINE, true, properties);
+    public WolfArmorItem(Holder<ArmorMaterial> armorMaterial, int armorDurabilityFactor) {
+        //super(armorMaterial, BodyType.CANINE, true, properties);
+        super (armorMaterial, BodyType.CANINE, true, new Item.Properties().durability(ArmorItem.Type.BODY.getDurability(armorDurabilityFactor)).stacksTo(1));
         String materialHolderName = armorMaterial.getRegisteredName();
         ResourceLocation resourceLocation = getArmorTexture(materialHolderName.substring(materialHolderName.lastIndexOf(":") + 1));
         WolfArmorPlus.LOGGER.debug(resourceLocation.toString());
